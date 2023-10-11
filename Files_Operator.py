92cbb2a76db1c67710abbbbdd69c834f6009dd5a
Meet <105229321+meet-v25@users.noreply.github.com>
2023-10-12 04:04:42 +0530

    Adding function to compress images

--- Files_Operator.py
+++ Files_Operator.py
@@ -1,8 +1,5 @@
-import os, cv2 as cv, numpy as np
+import os, cv2, numpy as np, numpy.linalg as npLA
 from shutil import rmtree
-
-
-
 
 ########################################################################################################################
 ################################################### HELPER FUNCTIONS ###################################################
@@ -14,8 +11,17 @@ def imshow(img,*args): # -> null
         for i in img: cv2.imshow(title,i); cv2.waitKey(0); cv2.destroyAllWindows(); 
     else: cv2.imshow(title,img); cv2.waitKey(0); cv2.destroyAllWindows(); 
 
-def helper_new(): pass
+def show_Channels(i1): # -> (R,G,B)
+        i2 = i1.copy(); i2[:,:,0]=0; i2[:,:,1]=0; # Show R 
+        i3 = i1.copy(); i3[:,:,0]=0; i3[:,:,2]=0; # Show G 
+        i4 = i1.copy(); i4[:,:,1]=0; i4[:,:,2]=0; # Show B 
+        imshow([i2,i3,i4]); 
+        return i2,i3,i4; 
 
+def shp(img): # -> (h,w,c)
+    s=img.shape; print(s); return s; 
+
+def helper_new(): pass
 
 
 ########################################################################################################################
@@ -49,12 +55,55 @@ def Crop_Img(pathh,coords):
 
 ########################################################################################################################
 
+def Compress_Single_Img(folder_pathh,img_namme,confirm_text_flag):
+    
+    pathh = os.path.join(folder_pathh,img_namme); ext = pathh[pathh.index("."):]; 
+    img_name_wo_ext = img_namme[:img_namme.index(".")]; new_dir = os.path.join(folder_pathh, img_name_wo_ext); 
+    if(os.path.isdir(new_dir)): rmtree(new_dir); 
+    os.mkdir(new_dir); 
+
+    old_img = cv2.imread(pathh); shp = old_img.shape; Us=[None,None,None]; Ss=[None,None,None]; Vs=[None,None,None]; 
+    for i in range(3): U,S,V = npLA.svd(old_img[:,:,i], full_matrices=False); Us[i]=U; Ss[i]=S; Vs[i]=V; 
+    
+    for k in range(1, min(shp[0],shp[1])):
+
+        new_img = np.zeros(shape=shp); 
+        for i in range(3): new_img[:,:,i] = Us[i][:,:k] @ (np.diag(Ss[i][:k]) @ (Vs[i][:k,:])); 
+        
+        new_img = new_img.astype('uint8'); 
+        new_path = os.path.join(new_dir, img_name_wo_ext + ('_'+('0'*(5-len(str(k))))+str(k)) + ext ); 
+        cv2.imwrite(new_path,new_img); 
+
+    if(confirm_text_flag):
+        print(f"\n'''''''''''''''''''''''''''''''''''''''''Done'''''''''''''''''''''''''''''''''''''''''"); 
+        print(f"Multiple compressed images are saved there, in a folder with the image's name."); 
+        print(f"________________________________________________________________________________________"); 
+
+def Compress_Imgs_in_Folder(pathh):
+    for filename in os.listdir(pathh):
+        if(os.path.isfile(os.path.join(pathh,filename))):
+            if (1-( filename.endswith("jfif") or filename.endswith("jfif") )):
+                Compress_Single_Img(folder_pathh=pathh, img_namme=filename, confirm_text_flag=0); 
+    
+    print(f"\n'''''''''''''''''''''''''''''''''''''''''Done'''''''''''''''''''''''''''''''''''''''''"); 
+    print(f"Folders with multiple compressed images are saved there."); 
+    print(f"________________________________________________________________________________________"); 
+
+########################################################################################################################
+
+def new_fn():pass
+
+
+########################################################################################################################
 
 
 
 
 
-######################################## MAIN LOOPING CODE ########################################
+
+########################################################################################################################
+################################################## MAIN LOOPING CODE ###################################################
+########################################################################################################################
 
 while(1):
     
@@ -83,7 +132,13 @@ while(1):
             input("This operation is yet to be implemented. Please try any other option(s). Press Enter to continue."); 
         
         case 3:
-            input("This operation is yet to be implemented. Please try any other option(s). Press Enter to continue."); 
-                
+            folder_pathh = input(f"Copy paste path of the folder in which image is here : ").strip(); 
+            img_namme = input(f"Copy paste name image here along with extension : ").strip(); 
+            Compress_Single_Img(folder_pathh=folder_pathh, img_namme=img_namme, confirm_text_flag=1); 
+        
+        case 4:
+            pathh = input(f"Copy paste path of the folder here : ").strip(); 
+            Compress_Imgs_in_Folder(pathh=pathh); 
+        
         case 0: break; 
         case _: input("Invalid choice. Press Enter and try again."); 
